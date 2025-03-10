6db992f47aaf4800753256eb2a735899a33f9675
P1zz <84851976+P1zz@users.noreply.github.com>
2025-03-10 21:55:54 +0100

    Cleaned some stuff

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/logging/log.h>
 
-//LOG_MODULE_REGISTER(main);
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   5000
 
 #define STRIP_NODE		DT_ALIAS(led_strip)
-
 #define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+
+#define USR_LED_RED 0
+#define USR_LED_GREEN 1
+#define USR_LED_BLUE 2
 
 #if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
 #define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
@@ -21,38 +21,37 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #endif
 #define DELAY_TIME K_MSEC(500)
 
-static const struct led_rgb colors[] = {
-	RGB(0x0f, 0x00, 0x00), /* red */
-	RGB(0x00, 0x0f, 0x00), /* green */
-	RGB(0x00, 0x00, 0x0f), /* blue */
+static struct led_rgb colors[] = {
+	RGB(0x08, 0x00, 0x00), /* red */
+	RGB(0x00, 0x08, 0x00), /* green */
+	RGB(0x00, 0x00, 0x08), /* blue */
 };
 
-static struct led_rgb pixels[STRIP_NUM_PIXELS];
 
 static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
-	int ret;
-	bool led_state = true;
-
-	size_t color = 2;
-	int rc;
-
-	printk("Hello World! %s\n", CONFIG_ARCH);
+	// printk("Hello World! %s\n", CONFIG_ARCH);
 	
 	if (device_is_ready(strip)) {
-		LOG_INF("Found LED strip device %s", strip->name);
+		LOG_INF("LED ready");
 	} else {
-		LOG_ERR("LED strip device %s is not ready", strip->name);
+		LOG_ERR("LED not ready");
 		return 0;
 	}
 
 	LOG_INF("Displaying pattern on strip");
 
+	for(uint8_t i = 0; i < 2; i++){
+		led_strip_update_rgb(strip, &colors[USR_LED_BLUE], STRIP_NUM_PIXELS);
+	}
+
+	LOG_INF("Entering while");
 	while (1) {
-		led_strip_update_rgb(strip, &colors[0], STRIP_NUM_PIXELS);
+		k_sleep(K_MSEC(500));
 	}
 	
+	LOG_INF("Exit main");
 	return 0;
 }
