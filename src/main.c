ba9ffa60f253f5bed6b5407c234606821dc3ec03
P1zz <84851976+P1zz@users.noreply.github.com>
2025-03-14 18:13:54 +0100

    Working dns

--- src/main.c
+++ src/main.c
@@ -9,6 +9,10 @@
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/dhcpv4.h>
 
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/dns_resolve.h>
+
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define STRIP_NODE		DT_ALIAS(led_strip)
@@ -82,7 +86,6 @@ static void ipv4_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt
 			LOG_INF("    Subnet[%d]: %s", net_if_get_by_iface(iface), net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[i].netmask, buf, sizeof(buf)));
 			LOG_INF("    Router[%d]: %s", net_if_get_by_iface(iface), net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
 			LOG_INF("Lease time[%d]: %u seconds", net_if_get_by_iface(iface), iface->config.dhcpv4.lease_time);
-	
 		}
     }
 }
@@ -134,6 +137,69 @@ static void get_wifi_rssi(void)
     }
 }
 
+void dns_result_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
+{
+	char hr_addr[NET_IPV6_ADDR_LEN];
+	char *hr_family;
+	void *addr;
+
+	switch (status) {
+	case DNS_EAI_CANCELED:
+		LOG_INF("DNS query was canceled");
+		return;
+	case DNS_EAI_FAIL:
+		LOG_INF("DNS resolve failed");
+		return;
+	case DNS_EAI_NODATA:
+		LOG_INF("Cannot resolve address");
+		return;
+	case DNS_EAI_ALLDONE:
+		LOG_INF("DNS resolving finished");
+		return;
+	case DNS_EAI_INPROGRESS:
+		break;
+	default:
+		LOG_INF("DNS resolving error (%d)", status);
+		return;
+	}
+
+	if (!info) {
+		return;
+	}
+
+	if (info->ai_family == AF_INET) {
+		hr_family = "IPv4";
+		addr = &net_sin(&info->ai_addr)->sin_addr;
+	} else if (info->ai_family == AF_INET6) {
+		hr_family = "IPv6";
+		addr = &net_sin6(&info->ai_addr)->sin6_addr;
+	} else {
+		LOG_ERR("Invalid IP address family %d", info->ai_family);
+		return;
+	}
+
+	LOG_INF("%s %s address: %s", user_data ? (char *)user_data : "<null>",
+		hr_family,
+		net_addr_ntop(info->ai_family, addr,
+					 hr_addr, sizeof(hr_addr)));
+}
+
+static void do_ipv4_lookup(void)
+{
+	static const char *query = "pool.ntp.org";
+	static uint16_t dns_id;
+	int ret;
+
+	ret = dns_get_addr_info(query, DNS_QUERY_TYPE_A, &dns_id, dns_result_cb, (void *)query, 2000);
+	if (ret < 0) {
+		LOG_ERR("Cannot resolve IPv4 address (%d)", ret);
+		return;
+	}
+
+	LOG_DBG("DNS id %u", dns_id);
+}
+
+
 int main(void)
 {
 	printk("_______       _____\n"); 
@@ -167,13 +233,16 @@ int main(void)
 	sta_iface = net_if_get_wifi_sta();
 	connect_to_wifi();
 
+	//Wait for connection
 	while(!wifi_connected){
-	k_sleep(K_MSEC(100));
+		k_sleep(K_MSEC(100));
 	}
 
 	LOG_INF("Wait 10s");
 	k_sleep(K_MSEC(10000));
 
+	do_ipv4_lookup();
+	
 	struct net_if *iface = net_if_get_default();
 
     if (!iface) {
@@ -183,9 +252,9 @@ int main(void)
 
 	while (1) {
 		k_sleep(K_MSEC(2000));
-		if (wifi_connected) {
-			get_wifi_rssi();
-		}
+		// if (wifi_connected) {
+		// 	get_wifi_rssi();
+		// }
 	}
 	
 	LOG_INF("Exit main");
